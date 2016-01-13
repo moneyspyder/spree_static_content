ae930f0df5ddf943a143a8ce92e16e14ab8d1dca
Warren Sangster <warren@moneyspyder.co.uk>
2016-01-13 12:31:04 +0000

    HH-25

--- app/models/spree/section.rb
+++ app/models/spree/section.rb
@@ -1,6 +1,6 @@
 class Spree::Section < ActiveRecord::Base
   belongs_to :page, class_name: 'Spree::Page'
 
-  validates :identifier, uniqueness: { case_sensitive: false, scope: 'page_id' }
+  validates :identifier, uniqueness: { case_sensitive: false, scope: 'page_id' }, presence: true
   validates :page, presence: true
 end
